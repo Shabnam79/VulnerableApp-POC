2743bf02a26a8010de8ca0e09415b8911d8601ae
Shabnam79 <109502045+Shabnam79@users.noreply.github.com>
2022-09-20 16:02:20 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,15 @@
-// {
-//     "chromeWebSecurity" : false,
-//     "$schema": "https://on.cypress.io/cypress.schema.json",
-//     "projectId": "ioceso",
-//     "experimentalStudio": true,
-//     "screenshotOnRunFailure": true,
-//     "reporter": "cypress-mochawesome-reporter",
-//     "reporterOptions": {
-//         "reportDir": "cypress/report",
-//         "charts": true,
-//         "reportPageTitle": "DEvSecOps",
-//         "embeddedScreenshots": true
-//       }
-// }
-const { defineConfig } = require("cypress");
+ {
+     "chromeWebSecurity" : false,
+     "$schema": "https://on.cypress.io/cypress.schema.json",
+     "projectId": "ioceso",
+     "experimentalStudio": true,
+     "screenshotOnRunFailure": true,
+     "reporter": "cypress-mochawesome-reporter",
+     "reporterOptions": {
+         "reportDir": "cypress/report",
+         "charts": true,
+         "reportPageTitle": "DEvSecOps",
+         "embeddedScreenshots": true
+       }
+ }
 
-module.exports = defineConfig({
-   e2e: {
-    setupNodeEvents(on, config) {
-       // implement node event listeners here
-   },
-//   },
-});
