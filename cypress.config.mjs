3b444c569a27b3364885db92c06a6594dc711102
Shabnam79 <109502045+Shabnam79@users.noreply.github.com>
2022-09-20 15:45:19 +0530

    Update and rename cypress.json to cypress.config.mjs

---
+++ cypress.config.mjs
@@ -0,0 +1,23 @@
+// {
+//     "chromeWebSecurity" : false,
+//     "$schema": "https://on.cypress.io/cypress.schema.json",
+//     "projectId": "ioceso",
+//     "experimentalStudio": true,
+//     "screenshotOnRunFailure": true,
+//     "reporter": "cypress-mochawesome-reporter",
+//     "reporterOptions": {
+//         "reportDir": "cypress/report",
+//         "charts": true,
+//         "reportPageTitle": "DEvSecOps",
+//         "embeddedScreenshots": true
+//       }
+// }
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
